278aabb9c9203b2a27b105e480d87a832b15b602
kvlknctk <iletisim@volkancatak.com.tr>
2019-05-05 17:47:00 +0300

    Dil sayfasında çalışmalar kısmı düzenlendi.

--- resources/lang/tr/seo.php
+++ resources/lang/tr/seo.php
@@ -6,7 +6,8 @@
 
             'home'     => 'Hidropar Eskişehir firmasının çalışmaları ve ürünlerinin yer aldığı resmi web sitesi.',
             'services' => 'Firmamızın sunmuş olduğu hizmetler hakkında detaylı bilgi alabilirsiniz. ',
-            'contact'  => 'İletişim bölümünden firmamızla iletişime geçebilir, teknik destek talebi oluşturabilir ve diğer konular hakkında bilgi alabilirsiniz. '
+            'contact'  => 'İletişim bölümünden firmamızla iletişime geçebilir, teknik destek talebi oluşturabilir ve diğer konular hakkında bilgi alabilirsiniz.',
+            'works'    => 'Hidropar Eskişehir Firmasının daha önceden yapmış olduğu çalışmalara sayfamızdan ulaşabilirsiniz.',
 
         ],
 
