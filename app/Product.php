34bea6097fe3cbbc198426213e03e2217e33ced9
kvlknctk <iletisim@volkancatak.com.tr>
2019-07-02 23:00:37 +0300

    Resim boyutlama ve popüler içerikler traiti eklendi

--- app/Product.php
+++ app/Product.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use JordanMiguel\LaravelPopular\Traits\Visitable;
+use TCG\Voyager\Traits\Resizable;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Product extends Model
 {
-    
+    use Resizable;
+    use Visitable;
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
