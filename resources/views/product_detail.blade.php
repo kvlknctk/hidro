78f4b24a30cc77edb572afba09dac10dae8f805f
kvlknctk <iletisim@volkancatak.com.tr>
2019-07-02 23:52:42 +0300

    Ürün detaylar sayfası ve ufak fix.

---
+++ resources/views/product_detail.blade.php
@@ -0,0 +1,262 @@
+@extends('layout.master')
+
+
+
+@section('content')
+    <section class="page-title" style="background-image:url({{asset('img/breadcumbs/urunler.jpg')}})">
+        <div class="auto-container">
+            <h1>{{SEOMeta::getTitleSession()}}</h1>
+            <ul class="page-breadcrumb">
+                <li><a href="{{route('index')}}">{{trans('seo.titles.home')}}</a></li>
+                <li><a href="{{route('products')}}">{{trans('seo.titles.products')}}</a></li>
+
+                @if(is_active(['urunler/*']))
+                    <li>{{SEOMeta::getTitleSession()}}</li>
+                @endif
+            </ul>
+        </div>
+    </section>
+
+
+    <div class="sidebar-page-container">
+        <div class="auto-container">
+            <div class="row clearfix">
+
+                <!--Content Side-->
+                <div class="content-side pull-right col-lg-9 col-md-8 col-sm-12 col-xs-12">
+                    <!--Shop Single-->
+                    <div class="shop-single">
+                        <div class="product-details">
+
+                            <!--Basic Details-->
+                            <div class="basic-details">
+                                <div class="row clearfix">
+                                    <div class="image-column col-md-6 col-sm-6 col-xs-12">
+                                        <figure class="image-box">
+                                            <a href="{{Voyager::image($product->image)}}" class="lightbox-image" title="{{$product->title}}">
+                                                <img src="{{Voyager::image($product->image)}}" alt="">
+                                            </a>
+                                        </figure>
+                                    </div>
+                                    <div class="info-column col-md-6 col-sm-6 col-xs-12">
+                                        <div class="details-header">
+                                            <h4>{{$product->title}}</h4>
+                                            {{--<div class="rating">
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star-half-empty"></span>
+                                            </div>--}}
+                                            {{--<div class="item-price">$29.00</div>--}}
+                                        </div>
+
+                                        <div class="text">
+                                            <p>{{$product->short_description}}</p>
+                                        </div>
+
+                                        {{--<div class="expired">Expected Delivery in 4-10 Days</div>
+                                        <div class="other-options clearfix">
+                                            <div class="item-quantity"><input class="quantity-spinner" type="text" value="2" name="quantity"></div>
+                                            <button type="button" class="theme-btn btn-style-one add-to-cart">Add To Cart</button>
+                                        </div>--}}
+                                    </div>
+                                </div>
+                            </div>
+                            <!--Basic Details-->
+
+                            <!--Product Info Tabs-->
+                            <div class="product-info-tabs">
+                                <!--Product Tabs-->
+                                <div class="prod-tabs tabs-box">
+
+                                    <!--Tab Btns-->
+                                    <ul class="tab-btns tab-buttons clearfix">
+                                        <li data-tab="#prod-details" class="tab-btn active-btn">Açıklama</li>
+                                        {{--<li data-tab="#prod-reviews" class="tab-btn ">Reviews (2)</li>--}}
+                                    </ul>
+
+                                    <!--Tabs Container-->
+                                    <div class="tabs-content">
+
+                                        <!--Tab -->
+                                        <div class="tab active-tab" id="prod-details">
+                                            <div class="content">
+                                                {!! $product->description !!}
+                                            </div>
+                                        </div>
+
+                                        <!--Tab / Active Tab-->
+                                        {{--<div class="tab active-tab" id="prod-reviews">
+                                            <h2 class="title">2 Reviews For win Your Friends</h2>
+                                            <!--Reviews Container-->
+                                            <div class="comments-area">
+                                                <!--Comment Box-->
+                                                <div class="comment-box">
+                                                    <div class="comment">
+                                                        <div class="author-thumb"><img src="images/resource/author-1.jpg" alt=""></div>
+                                                        <div class="comment-inner">
+                                                            <div class="comment-info clearfix">Steven Rich – Sep 17, 2016:</div>
+                                                            <div class="rating">
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star light"></span>
+                                                            </div>
+                                                            <div class="text">How all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings.</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <!--Comment Box-->
+                                                <div class="comment-box">
+                                                    <div class="comment">
+                                                        <div class="author-thumb"><img src="images/resource/author-2.jpg" alt=""></div>
+                                                        <div class="comment-inner">
+                                                            <div class="comment-info clearfix">William Cobus – Aug 21, 2016:</div>
+                                                            <div class="rating">
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star"></span>
+                                                                <span class="fa fa-star-half-empty"></span>
+                                                            </div>
+                                                            <div class="text">Working from home meant we couldsnack & coffee breaks change our desks or view good, drink on the job even spend  getting roughday in pajamas consequences.</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <!-- Comment Form -->
+                                            <div class="shop-comment-form">
+                                                <h2>Add Your Reviews</h2>
+                                                <div class="rating-box">
+                                                    <div class="text"> Your Rating:</div>
+                                                    <div class="rating">
+                                                        <a href="#">
+                                                            <span class="fa fa-star"></span>
+                                                        </a>
+                                                    </div>
+                                                    <div class="rating">
+                                                        <a href="#">
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                        </a>
+                                                    </div>
+                                                    <div class="rating">
+                                                        <a href="#">
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                        </a>
+                                                    </div>
+                                                    <div class="rating">
+                                                        <a href="#">
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                        </a>
+                                                    </div>
+                                                    <div class="rating">
+                                                        <a href="#">
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                            <span class="fa fa-star"></span>
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                                <form method="post" action="contact.html">
+                                                    <div class="row clearfix">
+                                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group">
+                                                            <label>First Name *</label>
+                                                            <input type="text" name="username" placeholder="" required>
+                                                        </div>
+
+                                                        <div class="col-md-6 col-sm-6 col-xs-12 form-group">
+                                                            <label>Last Name*</label>
+                                                            <input type="email" name="email" placeholder="" required>
+                                                        </div>
+                                                        <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+                                                            <label>Email*</label>
+                                                            <input type="text" name="number" placeholder="" required>
+                                                        </div>
+                                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group">
+                                                            <label>Your Comments*</label>
+                                                            <textarea name="message" placeholder=""></textarea>
+                                                        </div>
+
+                                                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group">
+                                                            <button class="theme-btn btn-style-one" type="submit" name="submit-form">Submit now</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>--}}
+                                    </div>
+                                </div>
+                            </div>
+                            <!--End Product Info Tabs-->
+
+                            <!--Related Products-->
+                            <div class="related-products">
+                                <h2>Benzer Ürünler</h2>
+                                <div class="row clearfix">
+
+                                    @foreach($related as $prod)
+
+
+                                        <div class="shop-item col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                                            <div class="inner-box">
+                                                <div class="image-box">
+                                                    <div class="image">
+                                                        <img src="{{Voyager::image($prod->image)}}" alt="" />
+                                                        <div class="overlay-box">
+                                                            <ul class="cart-option">
+                                                                <li><span class="tool-tip">Detay</span>
+                                                                    <a href="{{route('product_detail', ['category' => $prod->category->id, 'slug' => $prod->slug])}}"><span class="flaticon-search"></span></a>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="lower-content">
+                                                    <div class="price-box">
+                                                        <h3>
+                                                            <a href="{{route('product_detail', ['category' => $prod->category->id, 'slug' => $prod->slug])}}">{{$prod->title}}</a>
+                                                        </h3>
+                                                    </div>
+
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                    @endforeach
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!--Sidebar Side-->
+                <div class="sidebar-side col-lg-3 col-md-4 col-sm-12 col-xs-12">
+                    @include('parts.products_sidebar')
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+@endsection
+
+@push('scripts_orta')
+    <script src="{{asset('js/jquery.bootstrap-touchspin.js')}}"></script>
+@endpush
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <section class="page-title" style="background-image:url({{asset('img/breadcumbs/urunler.jpg')}})">
         <div class="auto-container">
-            <h1>Gönderildi</h1>
+            <h1>{{SEOMeta::getTitleSession()}}</h1>
             <ul class="page-breadcrumb">
                 <li><a href="{{route('index')}}">{{trans('seo.titles.home')}}</a></li>
                 <li><a href="{{route('products')}}">{{trans('seo.titles.products')}}</a></li>
@@ -48,7 +48,7 @@
                                                 <img src="{{Voyager::image($product->getThumbnail($product->image, 'thumb'))}}" alt="" />
                                                 <div class="overlay-box">
                                                     <ul class="cart-option">
-                                                        <li><span class="tool-tip">Detay</span><a href="{{route('product_detail', ['category' => $product->category->id, 'slug' => $product->slug])}}"><span class="flaticon-supermarket"></span></a></li>
+                                                        <li><span class="tool-tip">Detay</span><a href="{{route('product_detail', ['category' => $product->category->id, 'slug' => $product->slug])}}"><span class="flaticon-search"></span></a></li>
                                                     </ul>
                                                 </div>
                                             </div>
@@ -58,9 +58,6 @@
                                                 <h3><a href="{{route('product_detail', ['category' => $product->category->id, 'slug' => $product->slug])}}">{{$product->title}}</a></h3>
                                                 <div class="price">{{$product->category->title}}</div>
                                             </div>
-
-
-
                                         </div>
                                     </div>
                                 </div>
@@ -85,70 +82,11 @@
 
 
                 <div class="sidebar-side col-lg-3 col-md-4 col-sm-12 col-xs-12">
-                    <aside class="sidebar shop-sidebar">
-
-
-                        <div class="sidebar-widget search-box style-two">
-                            <form method="post" action="contact.html">
-                                <div class="form-group">
-                                    <input type="search" name="search-field" value="" placeholder="Search..." required>
-                                    <button type="submit"><span class="icon fa fa-search"></span></button>
-                                </div>
-                            </form>
-                        </div>
-
-
-                        <div class="sidebar-widget sidebar-blog-category">
-                            <div class="sidebar-title">
-                                <h2>Kategoriler</h2>
-                            </div>
-                            <ul class="blog-cat">
-                                <li class="{{active(['urunler'], 'active')}}"><a href="{{route('products')}}" >Tüm Ürünler</a></li>
-
-                                @foreach($categories as $category)
-                                    <li class="{{active(['urunler/'.$category->id], 'active')}}">
-                                        <a href="{{route('products_category', ['category' => $category->id])}}">{{$category->title}}</a>
-                                    </li>
-                                @endforeach
-
-                            </ul>
-                        </div>
-
-
-                        <div class="sidebar-widget related-posts">
-                            <div class="sidebar-title">
-                                <h2>Popüler Ürünler</h2>
-                            </div>
-
-
-                            @foreach($popular as $item)
-
-
-                                <div class="post">
-                                    <figure class="post-thumb">
-                                        <a href="{{route('product_detail', ['category' => $item->category->id, 'slug' => $item->slug])}}">
-                                            <img src="{{Voyager::image($item->getThumbnail($item->image, 'thumb'))}}" alt="">
-                                        </a>
-                                    </figure>
-                                    <h4><a href="{{route('product_detail', ['category' => $item->category->id, 'slug' => $item->slug])}}">{{$item->title}}</a></h4>
-                                    <div class="price">{{$item->category->title}}</div>
-                                </div>
-
-                            @endforeach
-
-
-
-
-                        </div>
-                    </aside>
+                    @include('parts.products_sidebar')
                 </div>
 
             </div>
         </div>
     </div>
-
-
-
-
 
 @endsection
